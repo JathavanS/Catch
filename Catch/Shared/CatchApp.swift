8f547e50619f34ef2af51f0f78715bd973a6bc2b
administrator <administrator@administrators-Mac-mini.local>
2022-09-11 22:00:45 -0400

    test commit

---
+++ Catch/Shared/CatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchApp.swift
+//  Shared
+//
+//  Created by administrator on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
